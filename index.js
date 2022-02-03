57839f02ecff28f0ca774b250f3a6dee8f83a3cd
Renzo <reemer2002@gmail.com>
2022-02-03 13:50:34 +0100

    bugs fixed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function init(){
     game = new Game();
 }
 
-class player {
+class Player {
     constructor(index){
         this.index = index;
         this.atTile = 0;
@@ -24,7 +24,7 @@ class Game{
     constructor(){
         this.selectplayersDiv = document.getElementsByClassName("selectplayers")[0];
         this.winnerDiv = document.getElementsByClassName("winner")[0];
-        this.playerturnDiv = document.getElementsByClassName("playerturn")[0];
+        this.playerturnDiv = document.getElementsByClassName("playerTurn")[0];
         this.rollDiv = document.getElementsByClassName("roll")[0];
         this.mainDiv = document.getElementsByClassName("main")[0];
         this.boardDiv = document.getElementsByClassName("board")[0];
