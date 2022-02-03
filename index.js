1bf17c4d2a1b4a973f170853d36eb32943e7ced2
Renzo <reemer2002@gmail.com>
2022-02-03 12:00:19 +0100

    er is iets fout maar ik kan niet vinden wat

--- index.js
+++ index.js
@@ -79,37 +79,73 @@ class Game{
     }
 
     start(amountOfPlayers){
-        //select player hide
-        //winner hide
+        this.selectplayersDiv.style.display = "none";
+        this.winnerDiv.style.display = "none";
+        this.mainDiv.style.display = "block";
 
-        //pawns alle 4 hide -> selectie class pawn
+        let pawns = document.getElementsByClassName("pawn")
 
-        //players aanmaken
+        for (var i = 0; i < pawns.length; i++){
+            pawns[i].style.display = "none";
+        }
 
-        //eerste beurt starten
-        //playerturn = -1
+        this.player = [];
+
+        for (var i = 0; i < amountOfPlayers; i++){
+            let player = new Player(i);
+            this.players.push(player);
+        }
+
+        this.playerturn = -1;
+        this.moveToNextPlayer();
+
     }
 
-    moveToNextPlayer(){
-        //player turn ophogen naar de volgende speler
-        //player turn groter dan aantal spelers?
-        // dan willen we terug naar eerste speler
+    moveToNextPlayer(){ 
+        this.playerturn++;
+        if(this.playerturn == this.players.length){
+            this.playerturn = 0;
+        }
 
-        //draw aanroepen
+        this.playerturnDiv.textContent = "player " + (this.playerturn + 1);
+
+        this.draw();
     }
 
     draw(){
-        //voor elke speler
-        //pawn neerzetten op huidige locatie van speler
-        //set pawn
+        for (var i = 0; i < this.players.length; i++){
+            this.setPawn(i, this.players[i].atTile);
+        }
     }
 
     roll(){
+        let eyes = Math.floor(Math.random() * 6) + 1;
+        this.rollDiv.style.backgroundImage = "url('img/dice" + eyes + ".png')";
+        let player = this.players[this.playerturn];
+        player.atTile += eyes;
 
+        if (player.atTile >= 49){
+            this.winnerDiv.style.display = "block";
+            this.winnerDiv.textContent = "player " + (this.playerturn + 1) + "wins!";
+            this.mainDiv.style.display = "none";
+            return;
+        }
+
+        this.draw();
+
+        let tile = this.tiles[player.atTile];
+        if(tile.goto != -1){
+            player.atTile = tile.goto;
+            this.draw();
+        }
+        this.moveToNextPlayer();
     }
 
     setPawn(playerI, atTile){
-
+        let tile = this.tiles[atTile];
+        let player = this.players[playerI];
+        player.pawn.style.left = tile.div.style.left;
+        player.pawn.style.top = tile.div.style.top;
     }
 
     makeBoardDiv(x, y, tileDisplayNumber){
